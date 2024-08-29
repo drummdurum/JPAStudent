8f145b5b7363028095d67618a7f9505af42a3053
drummdurum <147395124+drummdurum@users.noreply.github.com>
2024-08-29 10:21:12 +0200

    push prøve

---
+++ src/main/java/com/example/jpastudent/config/InitData.java
@@ -0,0 +1,15 @@
+package com.example.jpastudent.config;
+
+import com.example.jpastudent.model.Student;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitData implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+    Student s1 = new Student();
+
+
+    }
+}
